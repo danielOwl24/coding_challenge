24cbaaa56637961706cf694e9246711404b76896
Daniel Diaz <daniel_diaz@192.168.0.100>
2025-03-09 14:57:20 -0500

    creating management errors logic

--- src/utils.py
+++ src/utils.py
@@ -10,15 +10,15 @@ def load_csv_to_db(file_path, file_name):
         "jobs.csv": Jobs,
         "hired_employees.csv": HiredEmployees
     }
-
-    if file_name in model_mapping:
-        model_class = model_mapping[file_name]
-        fields = model_class.get_columns()
-        df = pd.read_csv(file_path, header=None, names=fields)
-    else:
-        logger.error("The file name is not mapped in our files dictionary.")
-        return {"error": "File not mapped"}, 500
-    try:
+    try: 
+        if file_name in model_mapping:
+            model_class = model_mapping[file_name]
+            fields = model_class.get_columns()
+            df = pd.read_csv(file_path, header=None, names=fields)
+        else:
+            logger.error("The file name is not mapped in our files dictionary.")
+            return {"error": "File not mapped."}, 500
+        
         stmt = insert(model_class).values(df.to_dict(orient="records"))
         stmt = stmt.on_conflict_do_update(
             index_elements = model_class.get_primary_key(),
@@ -26,8 +26,13 @@ def load_csv_to_db(file_path, file_name):
         )
         db.session.execute(stmt)
         db.session.commit()
-        return {"message": "Data loaded successfully"}, 200
+        return {"message": "Data loaded successfully."}, 200
+    except pd.errors.EmptyDataError:
+        return {"error": "The csv file is empty."}, 400
+    except pd.errors.ParserError:
+        return {"error:" "Error parsing the csv file."}, 400
     except IntegrityError:
-        print(IntegrityError)
         db.session.rollback()
-        return {"error": "Database error"}, 500
+        return {"error": "Database error."}, 500
+    except Exception as e:
+        return {"error": str(e)}, 500
